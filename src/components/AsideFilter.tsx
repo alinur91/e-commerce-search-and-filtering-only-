8f81e2c297aecc3a7ed316bf6eb51b0a551e5f93
Alisher Nurlybay <areshil91@gmail.com>
2023-12-30 23:37:40 +0600

    px gets smaller on sidebar

--- src/components/AsideFilter.tsx
+++ src/components/AsideFilter.tsx
@@ -2,7 +2,7 @@ import FilterByForm from "./FilterByForm";
 
 const AsideFilterBy = () => {
   return (
-    <div className="py-8 border-t-2 border-t-gray-200 w-full px-8 sm:pl-16 flex flex-col  gap-6">
+    <div className="py-8 border-t-2 border-t-gray-200 w-full px-2 sm:pl-16 flex flex-col  gap-6">
       <FilterByForm name="category" />
       <FilterByForm name="price" />
       <FilterByForm name="colors" />
